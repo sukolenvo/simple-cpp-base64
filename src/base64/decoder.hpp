7288fb0d5db05b39edb8a57f092b957bd72cfacc
Vadym <sukolenvo@gmail.com>
2023-08-09 19:42:06 +0800

    option BUILD_SHARED_LIBS

--- src/base64/decoder.hpp
+++ src/base64/decoder.hpp
@@ -4,8 +4,10 @@
 #include <string>
 #include <string_view>
 
+#include <SimpleCppBase64_Export.h>
+
 namespace simple_cpp::base64 {
-std::string decode(const std::string_view &input);
+std::string SimpleCppBase64_EXPORT decode(const std::string_view &input);
 }
 
 #endif // SIMPLE_CPP_BASE64_DECODER_HPP
